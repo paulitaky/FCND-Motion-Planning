cc437af61fe3d7a65acd659931e79c528f65549f
Paula <paula.kysela@gmail.com>
2020-05-28 16:00:18 +0200

    Implemented motion planning algorithm using a probabilistic roadmap.

--- motion_planning.py
+++ motion_planning.py
@@ -1,11 +1,17 @@
+import sys
 import argparse
 import time
 import msgpack
 from enum import Enum, auto
 
 import numpy as np
+import pandas as pd
+import utm
+from sklearn.neighbors import KDTree
+import pickle
 
-from planning_utils import a_star, heuristic, create_grid
+import planning_utils as plu
+import networkx as nx
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
@@ -87,7 +93,8 @@ class MotionPlanning(Drone):
         print("waypoint transition")
         self.target_position = self.waypoints.pop(0)
         print('target position', self.target_position)
-        self.cmd_position(self.target_position[0], self.target_position[1], self.target_position[2], self.target_position[3])
+        self.cmd_position(self.target_position[0], self.target_position[1],
+                          self.target_position[2], self.target_position[3])
 
     def landing_transition(self):
         self.flight_state = States.LANDING
@@ -106,57 +113,166 @@ class MotionPlanning(Drone):
         self.stop()
         self.in_mission = False
 
-    def send_waypoints(self):
+    def send_waypoints(self, waypoints):
         print("Sending waypoints to simulator ...")
-        data = msgpack.dumps(self.waypoints)
+        data = msgpack.dumps(waypoints)
         self.connection._master.write(data)
+
+    def is_goal_position_within_grid_boundaries(self, lon, lat, alt, grid_north_offset, grid_east_offset):
+        proposed_global_goal_position = [lon, lat, alt]
+        potential_local_position = global_to_local(proposed_global_goal_position, self.global_home)
+
+        if (potential_local_position[0] < abs(grid_north_offset) and potential_local_position[0] > -abs(grid_north_offset)):
+            if (potential_local_position[1] < abs(grid_east_offset) and potential_local_position[1] > -abs(grid_east_offset)):
+                return True, potential_local_position
+        return False, []
+
+    def set_home_position_from_colliders(self, filepath):
+        first_row = pd.read_csv(filepath, nrows=1)
+        lon0 = float(first_row.columns[1].split()[1])
+        lat0 = float(first_row.columns[0].split()[1])
+        alt0 = 0.0
+        self.set_home_position(lon0, lat0, alt0)
+
+    def get_grid_start_and_goal_position(self, input_data, target_altitude, 
+                                         safety_distance, 
+                                         use_gedoetic_frame_for_goal_position=True):
+        # Define a grid for a particular altitude and safety margin around obstacles
+        grid, north_offset, east_offset = plu.create_grid(input_data, target_altitude, safety_distance)
+
+        # retrieve current global position
+        current_global_position = [self._longitude, self._latitude, self._altitude]
+
+        # convert to current local position relative to new global home based on colliders.csv using global_to_local()
+        curr_local_position_rel_new_global_home = global_to_local(current_global_position, self.global_home)
+
+        # Define starting point on the grid
+        grid_start = (int(np.floor(curr_local_position_rel_new_global_home[0])), 
+                      int(np.floor(curr_local_position_rel_new_global_home[1])),
+                      curr_local_position_rel_new_global_home[2])
+
+        if use_gedoetic_frame_for_goal_position:
+            goal_position_valid = False
+            count = 0
+            max_proposals_allowed = 30
+            while not goal_position_valid or count < max_proposals_allowed:
+                goal_lon = np.random.uniform(-122.42, -122.3)
+                goal_lat = np.random.uniform(37.7, 37.82)
+                goal_position_valid, local_goal_position = self.is_goal_position_within_grid_boundaries(
+                    goal_lon, goal_lat, 0.0, north_offset, east_offset)
+
+                if (goal_position_valid):
+                    grid_goal = (int(np.floor(local_goal_position[0])), 
+                                 int(np.floor(local_goal_position[1])),
+                                 -target_altitude)
+                    break
+                else:
+                    count += 1
+            if not goal_position_valid:
+                print("no valid goal position found")
+        else:
+            # Set goal as some arbitrary position on the grid
+            goal_north_position = np.random.randint(0, 2 * abs(north_offset))
+            goal_east_position = np.random.randint(0, 2 * abs(east_offset))
+            grid_goal = (goal_north_position, goal_east_position, -target_altitude)
+
+        return grid, north_offset, east_offset, grid_start, grid_goal
 
     def plan_path(self):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
         TARGET_ALTITUDE = 5
-        SAFETY_DISTANCE = 5
+        SAFETY_DISTANCE = 3
 
         self.target_position[2] = TARGET_ALTITUDE
 
-        # TODO: read lat0, lon0 from colliders into floating point values
-        
-        # TODO: set home position to (lon0, lat0, 0)
+        # set home position to (lon0, lat0, 0) read from colliders.csv
+        self.set_home_position_from_colliders("colliders.csv")
 
-        # TODO: retrieve current global position
- 
-        # TODO: convert to current local position using global_to_local()
-        
-        print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
-                                                                         self.local_position))
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
-        
-        # Define a grid for a particular altitude and safety margin around obstacles
-        grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
-        print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
-        # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
-        # TODO: convert start position to current position rather than map center
-        
-        # Set goal as some arbitrary position on the grid
-        grid_goal = (-north_offset + 10, -east_offset + 10)
-        # TODO: adapt to set goal as latitude / longitude position and convert
 
-        # Run A* to find a path from start to goal
-        # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
-        # or move to a different search space such as a graph (not done here)
+        # set up grid, start and goal position
+        grid, north_offset, east_offset, grid_start, grid_goal = self.get_grid_start_and_goal_position(
+            data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print('Local Start and Goal: ', grid_start, grid_goal)
-        path, _ = a_star(grid, heuristic, grid_start, grid_goal)
-        # TODO: prune path to minimize number of waypoints
-        # TODO (if you're feeling ambitious): Try a different approach altogether!
+
+        # -------------- commented out if obstacle polygons imported -------------
+        # # extract polygons stating obstacles within the grid
+        # print("Extracting polygons from obstacles...")
+        # obstacle_polygons = plu.extract_polygons(data)
+        # with open('pickled_obstacle_polygons.pickle', 'wb') as f:
+        #     pickle.dump(obstacle_polygons, f)
+        # ------------------------------------------------------------------------
+
+        # -------------------- commented out if tree imported --------------------
+        # # setup kdtree to later find closest polygons of colliding sample points
+        # print("Setting up k-d-tree...")
+        # tree, base_polygon_0, base_polygon_1 = plu.build_kd_tree(obstacle_polygons)
+        # with open('pickled_tree.pickle', 'wb') as f:
+        #     pickle.dump(tree, f)
+        # ------------------------------------------------------------------------
+
+        # ------------------- commented out if graph imported --------------------
+        # # randomly select potential graph node locations within the grid
+        # print("Finding graph node locations...")
+        # np.random.seed(2)
+        # random_graph_node_locations = plu.sample_2d_locations(data, 1000, 30)
+        # t0 = time.time()
+        # # extracting valid graph nodes keeping the given safety distance from the obstacles
+        # valid_graph_node_locations = plu.get_valid_graph_node_locations(random_graph_node_locations, 
+        #                                                                 base_polygon_0, 
+        #                                                                 base_polygon_1, 
+        #                                                                 tree, 
+        #                                                                 obstacle_polygons,
+        #                                                                 5,
+        #                                                                 SAFETY_DISTANCE)
+        # print('It took {0} seconds to find valid graph nodes'.format(time.time() - t0))
+        # print("valid_graph_node_locations: ", len(valid_graph_node_locations))
+        # t1 = time.time()
+        # print("Creating graph...")
+        # G = plu.create_graph(obstacle_polygons, valid_graph_node_locations, 10)
+        # print('It took {0} minutes to build the graph'.format((time.time() - t1) / 60))
+        # print("Number of edges", len(G.edges))
+
+        # nx.write_gpickle(G, "pickled_graph_1000_random_nodes_30_zmax.pickle")
+        # ------------------------------------------------------------------------
+
+        # Read in obstacle polygons, tree and graph
+        print("Reading in obstacle polygons...")
+        with open("pickled_obstacle_polygons.pickle", 'rb') as f:
+            obstacle_polygons = pickle.load(f)
+        print("Reading in k-d-tree...")
+        with open("pickled_tree.pickle", 'rb') as f:
+            tree = pickle.load(f)
+        print("Reading in graph...")
+        graph_in = nx.read_gpickle("pickled_graph_1000_random_nodes_30_zmax.pickle")
+
+        # set start and goal of graph
+        print("Looking for a collision free start position...")
+        graph_start_nodes = plu.get_closest_nodes(graph_in, grid_start, 10)
+        start_connection_found, graph_start = plu.find_collision_free_node(
+            tree, 10, obstacle_polygons, graph_start_nodes, grid_start)
+        graph_goal = plu.get_closest_node(graph_in, grid_goal)
+
+        if not start_connection_found:
+            print("Cannot find collision free path to start location, please change position manually and try again.")
+            self.landing_transition()
+            self.disarming_transition()
+
+        print("Planning path...")
+        path, cost = plu.a_star(graph_in, plu.heuristic_graph, graph_start, graph_goal)
+        # print(len(path), path)
 
         # Convert path to waypoints
-        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
+        waypoints = [[p[0], p[1], p[2], 0] for p in path]
+
         # Set self.waypoints
         self.waypoints = waypoints
-        # TODO: send waypoints to sim (this is just for visualization of waypoints)
-        self.send_waypoints()
+
+        # Send waypoints to sim (this is just for visualization of waypoints)
+        waypoints_for_sim = [[int(p[0]), int(p[1]), int(p[2]), 0] for p in path]
+        self.send_waypoints(waypoints_for_sim)
 
     def start(self):
         self.start_log("Logs", "NavLog.txt")
--- planning_utils.py
+++ planning_utils.py
@@ -1,6 +1,15 @@
+from scipy.spatial import distance
+from shapely.geometry import Polygon, Point, LineString
+from sklearn.neighbors import KDTree
+import numpy.linalg as LA
+import networkx as nx
+
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
+import math
+import pkg_resources
+pkg_resources.require("networkx==2.1")
 
 
 def create_grid(data, drone_altitude, safety_distance):
@@ -31,12 +40,12 @@ def create_grid(data, drone_altitude, safety_distance):
         north, east, alt, d_north, d_east, d_alt = data[i, :]
         if alt + d_alt + safety_distance > drone_altitude:
             obstacle = [
-                int(np.clip(north - d_north - safety_distance - north_min, 0, north_size-1)),
-                int(np.clip(north + d_north + safety_distance - north_min, 0, north_size-1)),
-                int(np.clip(east - d_east - safety_distance - east_min, 0, east_size-1)),
-                int(np.clip(east + d_east + safety_distance - east_min, 0, east_size-1)),
+                int(np.clip(north - d_north - safety_distance - north_min, 0, north_size - 1)),
+                int(np.clip(north + d_north + safety_distance - north_min, 0, north_size - 1)),
+                int(np.clip(east - d_east - safety_distance - east_min, 0, east_size - 1)),
+                int(np.clip(east + d_east + safety_distance - east_min, 0, east_size - 1)),
             ]
-            grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
+            grid[obstacle[0]:obstacle[1] + 1, obstacle[2]:obstacle[3] + 1] = 1
 
     return grid, int(north_min), int(east_min)
 
@@ -88,59 +97,259 @@ def valid_actions(grid, current_node):
     return valid_actions
 
 
-def a_star(grid, h, start, goal):
+def get_valid_graph_node_locations(random_graph_node_locations, 
+                                   base_polygon_0,
+                                   base_polygon_1,
+                                   tree,
+                                   obstacle_polygons, 
+                                   no_of_neighbors,
+                                   safety_distance):
+    """ Gets valid (non obstacle colliding) locations for graph nodes within the input grid. """
+    valid_graph_node_locations = []
+    for point in random_graph_node_locations:
+        # use k nearest neighbor polygons in case representative points
+        # are that ill chosen that considered point is within one polygon
+        # but is checked with other (closest) polygon
+        if not collides(base_polygon_0,
+                        base_polygon_1, 
+                        tree, 
+                        obstacle_polygons, 
+                        point,
+                        no_of_neighbors=no_of_neighbors,
+                        safety_distance=safety_distance):
+            valid_graph_node_locations.append(point)
+
+    return valid_graph_node_locations
+
+
+def a_star(graph, h, start, goal):
+    """Modified A* to work with NetworkX graphs."""
 
     path = []
-    path_cost = 0
     queue = PriorityQueue()
     queue.put((0, start))
     visited = set(start)
 
     branch = {}
     found = False
-    
+
     while not queue.empty():
         item = queue.get()
+        current_cost = item[0]
         current_node = item[1]
-        if current_node == start:
-            current_cost = 0.0
-        else:              
-            current_cost = branch[current_node][0]
-            
+
         if current_node == goal:        
             print('Found a path.')
             found = True
             break
         else:
-            for action in valid_actions(grid, current_node):
-                # get the tuple representation
-                da = action.delta
-                next_node = (current_node[0] + da[0], current_node[1] + da[1])
-                branch_cost = current_cost + action.cost
-                queue_cost = branch_cost + h(next_node, goal)
-                
+            for next_node in graph[current_node]:
+                cost = graph.edges[current_node, next_node]['weight']
+                new_cost = current_cost + cost + heuristic(next_node, goal)
+
                 if next_node not in visited:                
                     visited.add(next_node)               
-                    branch[next_node] = (branch_cost, current_node, action)
-                    queue.put((queue_cost, next_node))
-             
+                    queue.put((new_cost, next_node))
+
+                    branch[next_node] = (new_cost, current_node)
+
+    path = []
+    path_cost = 0
     if found:
         # retrace steps
+        path = []
         n = goal
         path_cost = branch[n][0]
-        path.append(goal)
         while branch[n][1] != start:
             path.append(branch[n][1])
             n = branch[n][1]
         path.append(branch[n][1])
-    else:
-        print('**********************')
-        print('Failed to find a path!')
-        print('**********************') 
+
     return path[::-1], path_cost
 
+
+def extract_polygons(data):
+    polygons = []
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+
+        # Extract the 4 corners of the obstacle
+        lower_left = north - d_north, east - d_east
+        lower_right = north - d_north, east + d_east
+        upper_left = north + d_north, east - d_east
+        upper_right = north + d_north, east + d_east
+        corners = [(lower_left), (lower_right), (upper_right), (upper_left)]
+
+        height = alt + d_alt
+
+        p = Polygon(corners)
+        polygons.append((p, height))
+
+    return polygons
+
+
+def build_kd_tree_query(base_polygon_0, base_polygon_1, obj):
+    """ 
+    source: https://stackoverflow.com/questions/57273984/fast-way-to-find-the-closest-polygon-to-a-point/57274272?noredirect=1#comment101047819_57273984
+    """
+    dist_to_base_polygon_0 = base_polygon_0.distance(obj)
+    dist_to_base_polygon_1 = base_polygon_1.distance(obj)
+    return np.array([dist_to_base_polygon_0, dist_to_base_polygon_1])
+
+
+def build_kd_tree(obstacle_polygons, use_poly_point_representation=True):
+    if use_poly_point_representation:
+        polygon_point_representation = get_2d_polygons_for_tree(obstacle_polygons)
+        tree = KDTree(polygon_point_representation)
+        return tree, None, None
+    else:
+        # define base polygons for k-d-tree
+        base_polygon_0 = obstacle_polygons[0][0]
+        base_polygon_1 = obstacle_polygons[1][0]
+
+        distances = np.array([build_kd_tree_query(base_polygon_0, base_polygon_1, poly)
+                              for (poly, h) in obstacle_polygons])
+        tree = KDTree(distances)
+        return tree, base_polygon_0, base_polygon_1
+
+
+def get_2d_polygons_for_tree(polygons):
+    polygons_tree_representation = []
+    for (poly, h) in polygons:
+        polygons_tree_representation.append([poly.representative_point().x, poly.representative_point().y])
+    return polygons_tree_representation
+
+
+def sample_2d_locations(data, num_of_samples, z_max):
+    xmin = np.min(data[:, 0] - data[:, 3])
+    xmax = np.max(data[:, 0] + data[:, 3])
+
+    ymin = np.min(data[:, 1] - data[:, 4])
+    ymax = np.max(data[:, 1] + data[:, 4])
+
+    xvals = np.random.uniform(xmin, xmax, num_of_samples)
+    yvals = np.random.uniform(ymin, ymax, num_of_samples)
+    zvals = np.random.uniform(1.0, z_max, num_of_samples)
+
+    return list(zip(xvals, yvals, zvals))
+
+
+def collides(base_polygon_0, base_polygon_1, 
+             tree, polygons, point, 
+             no_of_neighbors, safety_distance):    
+    if base_polygon_0 == None or base_polygon_1 == None:
+        idx = tree.query([[point[0], point[1]]], k=no_of_neighbors, return_distance=False)[0]
+    else:
+        q = build_kd_tree_query(base_polygon_0, base_polygon_1, Point(point[0], point[1]))
+        idx = tree.query([q], k=no_of_neighbors, return_distance=False)[0]
+
+    for i in idx:
+        nearest_poly = polygons[i]
+        p = nearest_poly[0]
+        h = nearest_poly[1]
+        distance_to_border = p.distance(Point(point[0], point[1]))
+
+        if ((p.contains(Point(point)) and (h >= point[2])) or (safety_distance > distance_to_border)):
+            return True
+
+    return False
+
+
+def can_connect(polygons, p1, p2):
+    point_1 = (p1[0], p1[1])
+    point_2 = (p2[0], p2[1])
+    line = [point_1, point_2]
+    shapely_line = LineString(line)
+
+    for (poly, h) in polygons:
+        if (poly.crosses(shapely_line)) and h >= min(p1[2], p2[2]):
+            return False
+    return True
+
+
+def create_graph(obstacle_polygons, valid_graph_node_locations, k):
+    g = nx.Graph()
+    tree = KDTree(valid_graph_node_locations)
+
+    for n1 in valid_graph_node_locations:
+        # for each node connect try to connect to k nearest nodes
+        idxs = tree.query([n1], k, return_distance=False)[0]
+
+        for idx in idxs:
+            n2 = valid_graph_node_locations[idx]
+            if n2 == n1:
+                continue
+
+            if can_connect(obstacle_polygons, n1, n2):
+                g.add_edge(n1, n2, weight=1)
+    return g
+
+
+def get_closest_nodes(graph, current_point, no_of_nodes):
+    """
+    Find the k closest graph nodes to current_point.
+    """
+    node_distances = {}
+    dist = 100000
+    for p in graph.nodes:
+        d = LA.norm(np.array(p) - np.array(current_point))
+        node_distances[p] = d
+
+    closest_nodes = sorted(node_distances, key=node_distances.get)
+    closest_nodes = closest_nodes[0:(no_of_nodes - 1)]
+
+    return closest_nodes
+
+
+def get_closest_node(graph, point):
+    """
+    Find the closest graph node to certain point.
+    """
+    min_dist = 100000
+    closest_node = (0, 0)
+    for node in graph:
+        dist_curr = distance.euclidean(node, point)
+        if (dist_curr < min_dist):
+            min_dist = dist_curr
+            closest_node = node
+
+    return closest_node
+
+
+def find_collision_free_node(polygon_tree, no_of_neighbors, 
+                             obstacle_polygons, potential_nodes, 
+                             grid_point):
+    """
+    Finds the closest graph node to grid_point in the subset of potential_nodes 
+    that can be reached without collision.
+    """
+    connection_found = False
+    valid_node = None
+
+    for node in potential_nodes:
+        # get k nearest polygons to the grid point (node location could be chosen as well)
+        idx_grid_point_closest_polygons = polygon_tree.query(
+            [[grid_point[0], grid_point[1]]], k=no_of_neighbors, return_distance=False)[0]
+
+        # check if the drone can fly from grid point to graph node without collision
+        for i in idx_grid_point_closest_polygons:
+            if (can_connect([obstacle_polygons[i]], grid_point, node)):
+                connection_found = True
+                valid_node = node
+                break
+
+            if connection_found:
+                break
+    return connection_found, valid_node
 
 
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
 
+
+def heuristic_handcrafted(position, goal_position):
+    return math.sqrt(math.pow(position[0] - goal_position[0], 2) + math.pow(position[1] - goal_position[1], 2))
+
+
+def heuristic_graph(n1, n2):
+    return LA.norm(np.array(n2) - np.array(n1))
